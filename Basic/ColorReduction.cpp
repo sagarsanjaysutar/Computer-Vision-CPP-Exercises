eba9bc230ef1e593d108c3b3184dc92088feb66e
Sagar <sutarsagarsanjay@gmail.com>
2022-03-21 08:48:39 +0530

    Added Color Reduction program.

---
+++ Basic/ColorReduction.cpp
@@ -0,0 +1,101 @@
+#include <opencv2/core.hpp>
+#include <opencv2/core/utility.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <sstream>
+
+/*!
+    Refer: https://docs.opencv.org/3.4/db/da5/tutorial_how_to_scan_images.html
+*/
+
+cv::Mat &ScanImageAndReduceC(cv::Mat &I, const uchar *table);
+void printTable(const uchar (&table)[256]);
+
+int main()
+{
+    std::string imgPath("/home/sagar/Pictures/doge.jpg"); // Path to image.
+    int divideWith = 30;                                  // Number to divide the image pixels with.
+    uchar table[256];                                     // Table containing a particular pixel value for a range of pixel values e.g. 0..10=0, 10..19=10, 20-29= 20, etc.
+    cv::Mat I, J;
+
+    I = cv::imread(imgPath, cv::ImreadModes::IMREAD_COLOR);
+
+    if (I.empty())
+    {
+        std::cout << "Invalid image. Try another one." << std::endl;
+        return -1;
+    }
+
+    // Initialize the table
+    for (int pixelValue = 0; pixelValue < 256; ++pixelValue)
+    {
+        table[pixelValue] = (uchar)((pixelValue / divideWith) * divideWith); // Formula derived from OpenCV Site.
+    }
+
+    printTable(table);
+    cv::imshow("Original images", I);
+
+    cv::imshow("Reduced images", ScanImageAndReduceC(I, table));
+    cv::waitKey(0);
+
+    return 0;
+}
+/*!
+    @brief Reduces the image pixel intensity
+    @param Mat &I: reference to the Mat I in caller function. (Pass by reference as its a class and contains huge data)
+    @param const uchar* table: constant uchar table value. (Pass by pointer as it's a primitive type)
+    @return Mat &: Reference of mutated image matrix.
+    There isn't a need to return as the we are mutating a reference var. in the func., which by definition mutates the caller function's originally passed var. argumenet.
+*/
+cv::Mat &ScanImageAndReduceC(cv::Mat &I, const uchar *table)
+{
+    CV_Assert(I.depth() == CV_8U);
+
+    int nRows = I.rows;
+    int nCols = I.cols * I.channels();
+
+    // If the Matrix is stored in a continuos manner in the memory i.e. row0: BGR-BGR-..row1: BGR-BGR.., put it single row for easier read.
+    if (I.isContinuous())
+    {
+        nCols = nCols * nRows;
+        nRows = 1;
+    }
+
+    int i, j;
+    uchar *p;
+    for (i = 0; i < nRows; ++i)
+    {
+        p = I.ptr<uchar>(i); // Pointer extracted from reference var. I
+
+        for (j = 0; j < nCols; ++j)
+        {
+            p[j] = table[p[j]]; // Pixel value is replaced with the new table value e.g. table[189] => 180
+        }
+    }
+
+    return I;
+}
+
+/*!
+    @brief Prints the table array.
+    @param const uchar (&table)[256]: A constant uchar reference of table.
+
+    It's written this way because of array decay.
+    Check out https://www.geeksforgeeks.org/what-is-array-decay-in-c-how-can-it-be-prevented/
+*/
+void printTable(const uchar (&table)[256])
+{
+    int tableSize = (sizeof(table) / sizeof(table[0]));
+    std::stringstream sstream;
+    for (int i = 0; i < tableSize; ++i)
+    {
+        if (i != 0)
+            sstream << ",";
+
+        sstream << (uint)table[i];
+    }
+    std::string tableString = sstream.str();
+    std::cout << "[" << tableString << "]" << std::endl
+              << "Size: " << tableSize << std::endl;
+}
--- Basic/Matrix.cpp
+++ Basic/Matrix.cpp
@@ -8,27 +8,26 @@ int main(int, char **)
 
     // 2D Matrix
     cv::Mat A(twoDSize, CV_8U, cv::Scalar_<uint8_t>(10));
-    std::cout << A.size() << " sized, 8 bit uint matrix." << std::endl
+    std::cout << A.size() << " sized, 8 bit uint matrix with " << A.channels() << " color channel" << std::endl
               << cv::format(A, cv::Formatter::FMT_PYTHON) << std::endl
               << std::endl;
     cv::Mat B(twoDSize, CV_32FC1, cv::Scalar_<uint8_t>(96.25874));
-    std::cout << B.size() << " sized, 32 bit float matrix with 1 color channel." << std::endl
+    std::cout << B.size() << " sized, 32 bit float matrix with " << B.channels() << " color channel." << std::endl
               << cv::format(B, cv::Formatter::FMT_PYTHON) << std::endl
               << std::endl;
-    cv::Mat C(twoDSize, CV_32FC4, cv::Scalar(1.1, 0.2, 0.2, 0.2));
-    std::cout << C.size() << " sized, 32 bit float matrix with 4 color channel." << std::endl
+    cv::Mat C(twoDSize, CV_32FC3, cv::Scalar(1.1, 0.2, 3.2, 1.2));
+    std::cout << C.size() << " sized, 32 bit float matrix with " << C.channels() << " color channel." << std::endl
               << cv::format(C, cv::Formatter::FMT_PYTHON) << std::endl
               << std::endl;
 
-    // 3D Matrix
+    // 3D Matrix -- ????? Need to figure out
     cv::Mat D(3, threeDSize, CV_32FC4, cv::Scalar(1.1, 0.2, 0.2, 0.2));
-    std::cout << D.size() << "sized, 32 bit float matrix with 4 color channel." << std::endl;
-
-    for (int row = 0; row < (D.rows - 1); row++)
-    {
-        cv::Mat DSlice(row, D.cols, CV_32FC4, D.ptr());
-        std::cout << cv::format(DSlice, cv::Formatter::FMT_PYTHON) << std::endl
-                  << std::endl;
-    }
+    std::cout << D.size() << "sized, 32 bit float matrix with " << D.channels() << " color channel." << std::endl;
+    // for (int row = 0; row < (D.rows - 1); row++)
+    // {
+    //     cv::Mat DSlice(row, D.cols, CV_32FC4, D.ptr());
+    //     std::cout << cv::format(DSlice, cv::Formatter::FMT_PYTHON) << std::endl
+    //               << std::endl;
+    // }
     return 0;
 }

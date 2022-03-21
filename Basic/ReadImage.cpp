84e4acbdc3c5f4a7f2eeb5a5528d4b6bc40bf4e5
Sagar <sutarsagarsanjay@gmail.com>
2022-03-21 09:04:07 +0530

    Removed bins.

--- Basic/ReadImage.cpp
+++ Basic/ReadImage.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-int main(int argc, char **argv)
+int main()
 {
-    /* Impletement this if you want to pass image as an argument.
-    if (argc != 2)
-    {
-        printf("Provide an image path please. e.g.: ./app /home/root/basic.png \n");
-        return -1;
-    }*/
     std::string imgPath("/home/sagar/Pictures/doge.jpg");
     cv::Mat image;
     image = cv::imread(imgPath, 1);
@@ -22,8 +16,6 @@ int main(int argc, char **argv)
     std::cout << "Size: " << imageSize.height << " - " << imageSize.width << std::endl;
     cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
     cv::imshow("Image", image);
-
     cv::waitKey(0);
-
     return 0;
 }
